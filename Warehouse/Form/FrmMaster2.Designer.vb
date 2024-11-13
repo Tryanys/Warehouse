7037130088b848d90fc4f9bde34bc13271a4d7c8
unknown <ryansulistyajati@gmail.com>
2024-11-13 11:32:18 +0700

    update

--- Warehouse/Form/FrmMaster2.Designer.vb
+++ Warehouse/Form/FrmMaster2.Designer.vb
@@ -32,8 +32,8 @@ Partial Class FrmMaster2
         Me.Label8 = New System.Windows.Forms.Label()
         Me.tb1 = New System.Windows.Forms.TextBox()
         Me.tb2 = New System.Windows.Forms.TextBox()
-        Me.tb3 = New System.Windows.Forms.TextBox()
         Me.tb4 = New System.Windows.Forms.TextBox()
+        Me.tb5 = New System.Windows.Forms.TextBox()
         Me.cb1 = New System.Windows.Forms.ComboBox()
         Me.cb2 = New System.Windows.Forms.ComboBox()
         Me.cb3 = New System.Windows.Forms.ComboBox()
@@ -41,6 +41,9 @@ Partial Class FrmMaster2
         Me.lbrec = New System.Windows.Forms.Label()
         Me.pb = New System.Windows.Forms.ProgressBar()
         Me.pt = New System.Windows.Forms.PictureBox()
+        Me.Browse = New System.Windows.Forms.Button()
+        Me.tb3 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
         CType(Me.pt, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -48,184 +51,196 @@ Partial Class FrmMaster2
         '
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Times New Roman", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(64, 30)
-        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Location = New System.Drawing.Point(48, 9)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(83, 26)
+        Me.Label1.Size = New System.Drawing.Size(66, 22)
         Me.Label1.TabIndex = 0
         Me.Label1.Text = "Label1"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(65, 86)
-        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Location = New System.Drawing.Point(49, 36)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(41, 17)
+        Me.Label2.Size = New System.Drawing.Size(32, 13)
         Me.Label2.TabIndex = 1
         Me.Label2.Text = "Kode"
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(65, 121)
-        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Location = New System.Drawing.Point(49, 64)
         Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(45, 17)
+        Me.Label3.Size = New System.Drawing.Size(35, 13)
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Nama"
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(65, 156)
-        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Location = New System.Drawing.Point(49, 127)
         Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(79, 17)
+        Me.Label4.Size = New System.Drawing.Size(61, 13)
         Me.Label4.TabIndex = 3
         Me.Label4.Text = "Kecamatan"
         '
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(65, 252)
-        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Location = New System.Drawing.Point(49, 205)
         Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(37, 17)
+        Me.Label5.Size = New System.Drawing.Size(29, 13)
         Me.Label5.TabIndex = 6
         Me.Label5.Text = "TAG"
         '
         'Label6
         '
         Me.Label6.AutoSize = True
-        Me.Label6.Location = New System.Drawing.Point(65, 222)
-        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Location = New System.Drawing.Point(49, 180)
         Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(54, 17)
+        Me.Label6.Size = New System.Drawing.Size(42, 13)
         Me.Label6.TabIndex = 5
         Me.Label6.Text = "NoTelp"
         '
         'Label7
         '
         Me.Label7.AutoSize = True
-        Me.Label7.Location = New System.Drawing.Point(65, 187)
-        Me.Label7.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label7.Location = New System.Drawing.Point(49, 152)
         Me.Label7.Name = "Label7"
-        Me.Label7.Size = New System.Drawing.Size(51, 17)
+        Me.Label7.Size = New System.Drawing.Size(39, 13)
         Me.Label7.TabIndex = 4
         Me.Label7.Text = "Alamat"
         '
         'Label8
         '
         Me.Label8.AutoSize = True
-        Me.Label8.Location = New System.Drawing.Point(64, 283)
-        Me.Label8.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label8.Location = New System.Drawing.Point(48, 230)
         Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(46, 17)
+        Me.Label8.Size = New System.Drawing.Size(36, 13)
         Me.Label8.TabIndex = 7
         Me.Label8.Text = "Akses"
         '
         'tb1
         '
-        Me.tb1.Location = New System.Drawing.Point(168, 86)
-        Me.tb1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tb1.Location = New System.Drawing.Point(126, 36)
         Me.tb1.Name = "tb1"
-        Me.tb1.Size = New System.Drawing.Size(241, 22)
+        Me.tb1.Size = New System.Drawing.Size(182, 20)
         Me.tb1.TabIndex = 8
         '
         'tb2
         '
-        Me.tb2.Location = New System.Drawing.Point(168, 117)
-        Me.tb2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tb2.Location = New System.Drawing.Point(126, 61)
         Me.tb2.Name = "tb2"
-        Me.tb2.Size = New System.Drawing.Size(241, 22)
+        Me.tb2.Size = New System.Drawing.Size(182, 20)
         Me.tb2.TabIndex = 9
-        '
-        'tb3
-        '
-        Me.tb3.Location = New System.Drawing.Point(168, 183)
-        Me.tb3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
-        Me.tb3.Name = "tb3"
-        Me.tb3.Size = New System.Drawing.Size(241, 22)
-        Me.tb3.TabIndex = 10
         '
         'tb4
         '
-        Me.tb4.Location = New System.Drawing.Point(168, 218)
-        Me.tb4.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.tb4.Location = New System.Drawing.Point(126, 149)
         Me.tb4.Name = "tb4"
-        Me.tb4.Size = New System.Drawing.Size(241, 22)
-        Me.tb4.TabIndex = 11
+        Me.tb4.Size = New System.Drawing.Size(182, 20)
+        Me.tb4.TabIndex = 10
+        '
+        'tb5
+        '
+        Me.tb5.Location = New System.Drawing.Point(126, 177)
+        Me.tb5.Name = "tb5"
+        Me.tb5.Size = New System.Drawing.Size(182, 20)
+        Me.tb5.TabIndex = 11
         '
         'cb1
         '
         Me.cb1.FormattingEnabled = True
-        Me.cb1.Location = New System.Drawing.Point(168, 150)
-        Me.cb1.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.cb1.Location = New System.Drawing.Point(126, 122)
         Me.cb1.Name = "cb1"
-        Me.cb1.Size = New System.Drawing.Size(241, 24)
+        Me.cb1.Size = New System.Drawing.Size(182, 21)
         Me.cb1.TabIndex = 12
         '
         'cb2
         '
         Me.cb2.FormattingEnabled = True
-        Me.cb2.Location = New System.Drawing.Point(168, 249)
-        Me.cb2.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.cb2.Location = New System.Drawing.Point(126, 202)
         Me.cb2.Name = "cb2"
-        Me.cb2.Size = New System.Drawing.Size(104, 24)
+        Me.cb2.Size = New System.Drawing.Size(79, 21)
         Me.cb2.TabIndex = 13
         '
         'cb3
         '
         Me.cb3.FormattingEnabled = True
-        Me.cb3.Location = New System.Drawing.Point(168, 279)
-        Me.cb3.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.cb3.Location = New System.Drawing.Point(126, 227)
         Me.cb3.Name = "cb3"
-        Me.cb3.Size = New System.Drawing.Size(104, 24)
+        Me.cb3.Size = New System.Drawing.Size(79, 21)
         Me.cb3.TabIndex = 14
         '
         'lv
         '
         Me.lv.HideSelection = False
-        Me.lv.Location = New System.Drawing.Point(16, 319)
-        Me.lv.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.lv.Location = New System.Drawing.Point(12, 259)
         Me.lv.Name = "lv"
-        Me.lv.Size = New System.Drawing.Size(659, 192)
+        Me.lv.Size = New System.Drawing.Size(495, 157)
         Me.lv.TabIndex = 15
         Me.lv.UseCompatibleStateImageBehavior = False
         '
         'lbrec
         '
         Me.lbrec.AutoSize = True
-        Me.lbrec.Location = New System.Drawing.Point(16, 518)
-        Me.lbrec.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.lbrec.Location = New System.Drawing.Point(12, 421)
         Me.lbrec.Name = "lbrec"
-        Me.lbrec.Size = New System.Drawing.Size(37, 17)
+        Me.lbrec.Size = New System.Drawing.Size(30, 13)
         Me.lbrec.TabIndex = 16
         Me.lbrec.Text = "Rec."
         '
         'pb
         '
-        Me.pb.Location = New System.Drawing.Point(596, 518)
-        Me.pb.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.pb.Location = New System.Drawing.Point(447, 421)
         Me.pb.Name = "pb"
-        Me.pb.Size = New System.Drawing.Size(80, 16)
+        Me.pb.Size = New System.Drawing.Size(60, 13)
         Me.pb.TabIndex = 17
         '
         'pt
         '
-        Me.pt.Location = New System.Drawing.Point(468, 33)
+        Me.pt.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pt.Location = New System.Drawing.Point(352, 25)
+        Me.pt.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.pt.Name = "pt"
-        Me.pt.Size = New System.Drawing.Size(207, 270)
-        Me.pt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pt.Size = New System.Drawing.Size(155, 193)
+        Me.pt.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.pt.TabIndex = 18
         Me.pt.TabStop = False
         '
+        'Browse
+        '
+        Me.Browse.Location = New System.Drawing.Point(395, 225)
+        Me.Browse.Name = "Browse"
+        Me.Browse.Size = New System.Drawing.Size(75, 23)
+        Me.Browse.TabIndex = 19
+        Me.Browse.Text = "Browse.."
+        Me.Browse.UseVisualStyleBackColor = True
+        '
+        'tb3
+        '
+        Me.tb3.Location = New System.Drawing.Point(126, 87)
+        Me.tb3.Name = "tb3"
+        Me.tb3.Size = New System.Drawing.Size(182, 20)
+        Me.tb3.TabIndex = 21
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(49, 90)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(57, 13)
+        Me.Label9.TabIndex = 20
+        Me.Label9.Text = "Id Gudang"
+        '
         'FrmMaster2
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(718, 545)
+        Me.ClientSize = New System.Drawing.Size(538, 443)
+        Me.Controls.Add(Me.tb3)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Browse)
         Me.Controls.Add(Me.pt)
         Me.Controls.Add(Me.pb)
         Me.Controls.Add(Me.lbrec)
@@ -233,8 +248,8 @@ Partial Class FrmMaster2
         Me.Controls.Add(Me.cb3)
         Me.Controls.Add(Me.cb2)
         Me.Controls.Add(Me.cb1)
+        Me.Controls.Add(Me.tb5)
         Me.Controls.Add(Me.tb4)
-        Me.Controls.Add(Me.tb3)
         Me.Controls.Add(Me.tb2)
         Me.Controls.Add(Me.tb1)
         Me.Controls.Add(Me.Label8)
@@ -245,9 +260,8 @@ Partial Class FrmMaster2
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
         Me.Name = "FrmMaster2"
-        Me.Text = "Form Master"
+        Me.Text = " "
         CType(Me.pt, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
@@ -264,8 +278,8 @@ Partial Class FrmMaster2
     Friend WithEvents Label8 As Label
     Friend WithEvents tb1 As TextBox
     Friend WithEvents tb2 As TextBox
-    Friend WithEvents tb3 As TextBox
     Friend WithEvents tb4 As TextBox
+    Friend WithEvents tb5 As TextBox
     Friend WithEvents cb1 As ComboBox
     Friend WithEvents cb2 As ComboBox
     Friend WithEvents cb3 As ComboBox
@@ -273,4 +287,7 @@ Partial Class FrmMaster2
     Friend WithEvents lbrec As Label
     Friend WithEvents pb As ProgressBar
     Friend WithEvents pt As PictureBox
+    Friend WithEvents Browse As Button
+    Friend WithEvents tb3 As TextBox
+    Friend WithEvents Label9 As Label
 End Class
--- Warehouse/Form/FrmMaster2.vb
+++ Warehouse/Form/FrmMaster2.vb
@@ -96,8 +96,8 @@ Public Class FrmMaster2
         Try
 
             tb2.Text = ""
-            tb3.Text = ""
             tb4.Text = ""
+            tb5.Text = ""
             cb1.Text = ""
             cb2.Text = ""
             cb3.Text = ""
@@ -108,13 +108,13 @@ Public Class FrmMaster2
             For Each dt As DataRow In cpro.ExecQuery(csql).Rows
                 tb2.Text = dt(0)
                 cb1.Text = dt(1)
-                tb3.Text = dt(2)
-                tb4.Text = dt(3)
+                tb4.Text = dt(2)
+                tb5.Text = dt(3)
                 cb2.Text = dt(4)
                 cb3.Text = dt(5)
                 img = dt(6)
                 Dim ms As New MemoryStream(img)
-                pt.Image = Image.FromStream(MS)
+                pt.Image = Image.FromStream(ms)
             Next
         Catch ex As Exception
             MsgBox(Err.Description, vbInformation, "Cek err")
@@ -153,13 +153,13 @@ Public Class FrmMaster2
 
     'End Sub
 
-    Private Sub FrmMaster2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tb1.KeyPress, tb2.KeyPress, cb1.KeyPress, tb3.KeyPress, tb4.KeyPress, cb2.KeyPress, cb3.KeyPress
+    Private Sub FrmMaster2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles tb1.KeyPress, tb2.KeyPress, cb1.KeyPress, tb4.KeyPress, tb5.KeyPress, cb2.KeyPress, cb3.KeyPress
         If e.KeyChar = Chr(13) Then
             If sender.Equals(tb1) Then tb2.Focus() : SendKeys.Send("{end}")
             If sender.Equals(tb2) Then cb1.Focus() : cb1.DroppedDown = True
-            If sender.Equals(cb1) Then tb3.Focus() : SendKeys.Send("{end}")
-            If sender.Equals(tb3) Then tb4.Focus() : SendKeys.Send("{end}")
-            If sender.Equals(tb4) Then cb2.Focus() : cb2.DroppedDown = True
+            If sender.Equals(cb1) Then tb4.Focus() : SendKeys.Send("{end}")
+            If sender.Equals(tb4) Then tb5.Focus() : SendKeys.Send("{end}")
+            If sender.Equals(tb5) Then cb2.Focus() : cb2.DroppedDown = True
             If sender.Equals(cb2) Then cb3.Focus() : cb3.DroppedDown = True
             If sender.Equals(cb3) Then
                 'simpan
@@ -169,15 +169,15 @@ Public Class FrmMaster2
             If sender.Equals(tb1) Then Me.Close()
             If sender.Equals(tb2) Then tb1.Focus() : SendKeys.Send("{end}")
             If sender.Equals(cb1) Then tb2.Focus() : SendKeys.Send("{end}")
-            If sender.Equals(tb3) Then cb1.Focus() : cb1.DroppedDown = True
-            If sender.Equals(tb4) Then tb3.Focus() : SendKeys.Send("{end}")
-            If sender.Equals(cb2) Then tb4.Focus() : SendKeys.Send("{end}")
+            If sender.Equals(tb4) Then cb1.Focus() : cb1.DroppedDown = True
+            If sender.Equals(tb5) Then tb4.Focus() : SendKeys.Send("{end}")
+            If sender.Equals(cb2) Then tb5.Focus() : SendKeys.Send("{end}")
             If sender.Equals(cb3) Then cb2.Focus() : cb2.DroppedDown = True
         End If
         e.KeyChar = HurufBesar(e.KeyChar)
     End Sub
 
-    Private Sub tb4_KeyDown(sender As Object, e As KeyEventArgs) Handles tb1.KeyDown, tb2.KeyDown, cb1.KeyDown, tb3.KeyDown, cb2.KeyDown, tb4.KeyDown, cb3.KeyDown
+    Private Sub tb4_KeyDown(sender As Object, e As KeyEventArgs) Handles tb1.KeyDown, tb2.KeyDown, cb1.KeyDown, tb4.KeyDown, cb2.KeyDown, tb5.KeyDown, cb3.KeyDown
         If e.Control = True And (e.KeyCode = Asc("S") Or e.KeyCode = Asc("s")) Then
             dtpro("sim")
         ElseIf e.Control = True And (e.KeyCode = Asc("D") Or e.KeyCode = Asc("d")) Then
@@ -190,8 +190,10 @@ Public Class FrmMaster2
     Private Sub dtpro(ByVal mpro As String)
         Try
             If mpro = "sim" Or mpro = "hap" Then
-
-                csql = "exec TokoMaster.dbo.sp_Master#3Pro '" & dttemp.ndKey & "','" & mpro & "','" & tb1.Text & "','" & cb1.Text & "','" & tb2.Text & "','" & tb3.Text & "','" & tb4.Text & "','" & cb2.Text & "','" & cb3.Text & "'"
+                Dim fotoPath As String = pt.Text
+                Dim fotoBytes() As Byte = File.ReadAllBytes(fotoPath)
+                Dim csql As String
+                csql = "exec TokoMaster.dbo.sp_Master#3Pro '" & dttemp.ndKey & "','" & mpro & "','" & tb1.Text & "','" & cb1.Text & "','" & tb2.Text & "','" & tb4.Text & "','" & tb5.Text & "','" & cb2.Text & "','" & cb3.Text & "','" & pt.Text & "'"
                 For Each dt As DataRow In cpro.ExecQuery(csql).Rows
                     MsgBox(dt("Ket"), vbInformation, "Cek Err")
                 Next
@@ -199,10 +201,10 @@ Public Class FrmMaster2
             ElseIf mpro = "bar" Then
                 tb1.Text = ""
                 tb2.Text = ""
-                tb3.Text = ""
+                tb4.Text = ""
                 cb1.Text = ""
                 cb2.Text = ""
-                tb4.Text = ""
+                tb5.Text = ""
                 cb3.Text = ""
                 tb1.Focus()
                 tampdt()
@@ -215,4 +217,19 @@ Public Class FrmMaster2
     Private Sub pt_Click(sender As Object, e As EventArgs) Handles pt.Click
 
     End Sub
+
+    Private Sub Browse_Click(sender As Object, e As EventArgs) Handles Browse.Click
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp"
+        ofd.Title = "Pilih Gambar"
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+            Dim imgPath As String = ofd.FileName
+            Dim img As Image = Image.FromFile(imgPath)
+            pt.Image = img
+        End If
+
+        ofd.Dispose()
+    End Sub
+
 End Class
